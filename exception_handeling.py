eb04d2b3aa171313f932d78834928bc103b93a75
bholeram07 <bholeram@gkmit.co>
2024-09-29 21:52:43 +0530

    exception_handelling added

---
+++ exception_handeling.py
@@ -0,0 +1,24 @@
+#zero devision error
+'''
+  python has 4 exceptions
+  1. try (execute)
+  2. except(if any error than execute)
+  3. else (if true than execute)
+  4. finally (always execute)
+'''
+
+#by class_object
+a=int(input("Enter the number 1 "))
+b=int(input("Enter the number 2 "))
+
+try:
+    div=a/b
+    print(div)
+except:
+    print("Zero Devision Error")
+
+else:
+    print("Successfully executed")
+
+
+    
